2517ea61c08c72167c6f04687687524d5ea2f2e7
Nutyummy87 <nutyummy87@gmail.com>
2024-12-06 15:49:02 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,7 +16,7 @@ let classifier;
 let label = "listening";
 
 // Teachable Machine model URL:
-let soundModelURL = 'https://teachablemachine.withgoogle.com/models/h3p9R41J/model.json';
+let soundModelURL = 'https://nutyummy87.github.io/frog11324129/model.json';
 
 
 function preload() {
